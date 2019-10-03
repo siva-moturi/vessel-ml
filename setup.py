2e0409413aae9ee0d5f927a61d264095d78c4270
siva-moturi <56124598+siva-moturi@users.noreply.github.com>
2019-10-03 10:00:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="vesselml",
-    version="0.0.7",
+    version="0.0.9",
     author="Siva Moturi",
     author_email="siva.moturi@pfizer.com",
     description="Vessel ML library for cloud based ML training and model deployment",
