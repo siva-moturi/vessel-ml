8604cec4fc23df270f239087f0a29c32cd80ff1f
siva-moturi <56124598+siva-moturi@users.noreply.github.com>
2019-10-03 09:49:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+setuptools.setup(
+    name="vesselml",
+    version="0.0.7",
+    author="Siva Moturi",
+    author_email="siva.moturi@pfizer.com",
+    description="Vessel ML library for cloud based ML training and model deployment",
+    url="http://bitbucket-insightsnow.pfizer.com:7990/scm/ga/vessel-ml.git",
+    packages=setuptools.find_packages(),
+    install_requires=[
+              'pandas',
+              'kubernetes',
+              'kfmd',
+              'fairing',
+              'google-cloud-storage',
+              'joblib',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
